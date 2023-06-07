28c57b36eebee777a828cfb16491ad21f293d7cc
yyyu <tsukoha3236@gmail.com>
2023-06-07 21:05:14 +0900

    [Add] vgg

---
+++ vgg.py
@@ -0,0 +1,10 @@
+import numpy as np
+import json
+from PIL import Image
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+from torchvision import models, transforms
+
+print("Pytorch Version: ", torch.__version__)
+print("Torchvision Version: ", torchvision.__version__)
